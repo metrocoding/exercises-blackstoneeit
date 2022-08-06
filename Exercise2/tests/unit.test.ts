c3458127f4c9fcc6fb1fa4573e95cc16bdc7d660
Armin <dead4game@yahoo.com>
2022-08-06 14:48:47 +0430

    add read me

--- Exercise2/tests/unit.test.ts
+++ Exercise2/tests/unit.test.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 import "jest";
 import { join } from "path";
-import { createCsvFile } from "../src";
+import { createCsvFile, filteringData, lineToObject } from "../src";
 
 describe("csv exercise", () => {
     it("sould create file successfully", async () => {
@@ -9,5 +9,16 @@ describe("csv exercise", () => {
         const path = join(__dirname, "./test.csv");
         const data = readFileSync(path, "utf8");
         expect(data.trim()).toEqual("color,name,brand");
+    });
+
+    it("should return correct object", () => {
+        const object = lineToObject("ID426632,   Willa Hollow,   Intelligent Copper Knife,   4,  Hilll-Gorczany");
+        expect(object.id).toEqual("ID426632");
+        expect(object.quantity).toEqual(4);
+    });
+
+    it("should do the filtering", () => {
+        const { csv1Data, csv2Data } = filteringData();
+        expect(csv1Data.length).toEqual(csv2Data.length);
     });
 });
